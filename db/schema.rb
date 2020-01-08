62798a470328d335bd874e1a1868ef645997ab62
Tekin Suleyman <tekin@tekin.co.uk>
2020-01-08 13:42:56 +0000

    Add School#phone_number and set on import
    We'll be showing this to service operators to help them contact the
    school for an identity check.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_120123) do
+ActiveRecord::Schema.define(version: 2020_01_08_133802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -191,6 +191,7 @@ ActiveRecord::Schema.define(version: 2020_01_06_120123) do
     t.date "close_date"
     t.integer "establishment_number"
     t.integer "statutory_high_age"
+    t.string "phone_number", limit: 20
     t.index ["created_at"], name: "index_schools_on_created_at"
     t.index ["local_authority_district_id"], name: "index_schools_on_local_authority_district_id"
     t.index ["local_authority_id"], name: "index_schools_on_local_authority_id"
